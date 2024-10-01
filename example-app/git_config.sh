eae585fcffe360112d1cfaa0f30d53ebec349d2d
alena_liskova <liskova.aja@gmail.com>
2024-10-01 08:03:14 +0200

    restart

---
+++ example-app/git_config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git config --list
+git config --global user.name "alena_liskova"
+git config --global user.email "liskova.aja@gmail.com"
+echo "Přihlášení uživatele proběhlo v pořádku"
+ls
+dir
+
+#cd eshop_nevim
+#php artisan serve
+#https://fonts.bunny.net
+#https://www.fontshare.com          //php artisan make:class
